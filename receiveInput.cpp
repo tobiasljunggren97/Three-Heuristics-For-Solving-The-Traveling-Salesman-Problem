e1fb9065258d7bd299b6dd6f1ffd6ab83e99a976
tljun <tljun@kth.se>
2023-10-26 13:37:45 +0200

    prims

--- receiveInput.cpp
+++ receiveInput.cpp
@@ -20,7 +20,7 @@ double calculateEuclidianDistance(const Point& p1, const Point& p2) {
 vector<vector<double> > receiveInput(int numPoints) {
     vector<Point> points(numPoints);
     //Initiera 2d array
-    vector<vector<double> > weights(numPoints, vector<double>(numPoints));
+    vector<vector<double> > weight(numPoints, vector<double>(numPoints));
 
     for (int i = 0; i < numPoints; ++i) {
         cin >> points[i].x >> points[i].y;
@@ -30,14 +30,79 @@ vector<vector<double> > receiveInput(int numPoints) {
         for (int j = i + 1; j < numPoints; ++j) {
             double distance = calculateEuclidianDistance(points[i], points[j]);
             // cout << "Distance between points " << i << " and " << j << ": " << distance << endl;
-            weights[i][j] = distance;
+            weight[i][j] = distance;
+            weight[j][i] = distance;
         }
     }
     //Kör alla heursiticis och shit
 
-    return weights;
+    return weight;
 
 };
+
+int prims(vector<vector<double> > weight, int numPoints){
+  int no_edge;  // number of edge
+
+  // create a array to track selected vertex
+  // selected will become true otherwise false
+  int selected[numPoints];
+
+  // set selected false initially
+  memset(selected, false, numPoints);
+
+  // set number of edge to 0
+  no_edge = 0;
+
+  // the number of egde in minimum spanning tree will be
+  // always less than (V -1), where V is number of vertices in
+  //graph
+
+  // choose 0th vertex and make it true
+  selected[0] = true;
+
+  int x;  //  row number
+  int y;  //  col number
+
+  // print for edge and weight
+  cout << "Edge"
+     << " : "
+     << "Weight";
+  cout << endl;
+  while (no_edge < numPoints - 1) {
+    //For every vertex in the set S, find the all adjacent vertices
+    // , calculate the distance from the vertex selected at step 1.
+    // if the vertex is already in the set S, discard it otherwise
+    //choose another vertex nearest to selected vertex  at step 1.
+
+    int min = 999999;
+    x = 0;
+    y = 0;
+
+    for (int i = 0; i < numPoints; i++) {
+      if (selected[i]) {
+        // cout << "selected[i]: " << i << endl;
+        for (int j = 0; j < numPoints; j++) {
+          if (!selected[j] && j != i) {  // not in selected and there is an edge
+            // cout << "selected[j] not selected found: " << j << endl;
+            if (min > weight[i][j] && i != j) {
+             cout << "new min: " << weight[i][j] << " between " << i << " and " << j << endl;
+              min = weight[i][j];
+              x = i;
+              y = j;
+            }
+          }
+        }
+      }
+    }
+    cout << x << " - " << y << " :  " << weight[x][y];
+    cout << endl;
+    selected[y] = true;
+    no_edge++;
+  }
+
+  return 0;    
+}
+
 
 int main() {
     // Read input
@@ -47,7 +112,17 @@ int main() {
     vector<double> inner_vector(N);
     vector<vector<double> > weight(N,inner_vector);
     weight = receiveInput(N);
-    //print weights
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cout << "Distance between points " << i << " and " << j << ": " << weight[i][j] << endl;
+        }
+        cout << endl;
+    }
+    
+
+    prims(weight, N);
 
     return 0;
 }
+
+
