2c12bd83528ceeada343ec28c2aea4b4f1aef9e6
Martin Lindefors <lindefor@kth.se>
2023-10-26 16:29:11 +0200

    Removed points struct and improved code

--- receiveInput.cpp
+++ receiveInput.cpp
@@ -6,29 +6,32 @@
 using namespace std;
 
 struct Point {
-    double x, y;
+    double x, y; //REMOVE
 };
 
-double calculateEuclidianDistance(const Point& p1, const Point& p2) {
-    double dx = p1.x - p2.x;
-    double dy = p1.y - p2.y; //CHANGE TO INTS HERE DO NOT FORGET
-    //Vill vi har roundat?
-    return round(sqrt(dx * dx + dy * dy));
+double calculateEuclidianDistance(const tuple<double, double>& p1, const tuple<double, double>& p2) {
+    double dx = get<0>(p1) - get<0>(p2);
+    double dy = get<1>(p1) - get<1>(p2); 
+    
+    return round(sqrt(dx * dx + dy * dy)); //Round as per instructions
 };
 
 
 vector<vector<double> > receiveInput(int numPoints) {
     vector<Point> points(numPoints);
+    vector<tuple<double, double>> ponts(numPoints);
     //Initiera 2d array
     vector<vector<double> > weight(numPoints, vector<double>(numPoints));
-
+    double point1, point2;
     for (int i = 0; i < numPoints; ++i) {
-        cin >> points[i].x >> points[i].y;
+        cin >> point1 >> point2;
+        ponts[i] = make_tuple(point1, point2);
+        // cin >> points[i].x >> points[i].y;
     }
 
     for (int i = 0; i < numPoints; ++i) {
         for (int j = i + 1; j < numPoints; ++j) {
-            double distance = calculateEuclidianDistance(points[i], points[j]);
+            double distance = calculateEuclidianDistance(ponts[i], ponts[j]);
             // cout << "Distance between points " << i << " and " << j << ": " << distance << endl;
             weight[i][j] = distance;
             weight[j][i] = distance;
@@ -74,7 +77,7 @@ vector<vector<int>> prims(vector<vector<double> > weight, int numPoints){
     
     adjacencyList[node1].push_back(node2);
     adjacencyList[node2].push_back(node1);
-    //cout << node1 << " - " << node2 << " :  " << weight[node1][node2];
+    // cout << node1 << " - " << node2 << " :  " << weight[node1][node2];
     cout << endl;
     handled[node2] = true;
     numEdges++;
@@ -92,24 +95,24 @@ int main() {
     vector<double> inner_vector(N);
     vector<vector<double> > weight(N,inner_vector);
     weight = receiveInput(N);
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            cout << "Distance between points " << i << " and " << j << ": " << weight[i][j] << endl;
-        }
-        cout << endl;
-    }
+    // for(int i = 0; i < N; i++){
+    //     for(int j = 0; j < N; j++){
+    //         cout << "Distance between points " << i << " and " << j << ": " << weight[i][j] << endl;
+    //     }
+    //     cout << endl;
+    // }
     
     vector<vector<int> > adjacencyList(N);
     adjacencyList = prims(weight, N);
 
     // Print adjacency list
-    // for (int i = 0; i < N; ++i) {
-    //     cout << i << ": ";
-    //     for (int j = 0; j < adjacencyList[i].size(); ++j) {
-    //         cout << adjacencyList[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
+    for (int i = 0; i < N; ++i) {
+        cout << i << ": ";
+        for (int j = 0; j < adjacencyList[i].size(); ++j) {
+            cout << adjacencyList[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
