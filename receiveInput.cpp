d08d0c114dd428a99088e357a5024b0fbd7a22b4
Martin Lindefors <lindefor@kth.se>
2023-10-26 15:54:49 +0200

    Improved prims and added return of adjacency list for the MST graph

--- receiveInput.cpp
+++ receiveInput.cpp
@@ -40,64 +40,47 @@ vector<vector<double> > receiveInput(int numPoints) {
 
 };
 
-int prims(vector<vector<double> > weight, int numPoints){
-  int no_edge;  // number of edge
+vector<vector<int>> prims(vector<vector<double> > weight, int numPoints){
+  int numEdges = 0;  
+  vector<int> handled(numPoints);
+  handled[0] = true;
+  vector<vector<int> > adjacencyList(numPoints);
 
-  // create a array to track selected vertex
-  // selected will become true otherwise false
-  vector<int> selected(numPoints);
+  int node1;  
+  int node2;  
 
-  // set number of edge to 0
-  no_edge = 0;
+  while (numEdges < numPoints - 1) {
 
-  // the number of egde in minimum spanning tree will be
-  // always less than (V -1), where V is number of vertices in
-  //graph
-
-  // choose 0th vertex and make it true
-  selected[0] = true;
-
-  int x;  //  row number
-  int y;  //  col number
-
-  // print for edge and weight
-  cout << "Edge"
-     << " : "
-     << "Weight";
-  cout << endl;
-  while (no_edge < numPoints - 1) {
-    //For every vertex in the set S, find the all adjacent vertices
-    // , calculate the distance from the vertex selected at step 1.
-    // if the vertex is already in the set S, discard it otherwise
-    //choose another vertex nearest to selected vertex  at step 1.
-
-    int min = 999999;
-    x = 0;
-    y = 0;
+    int min = numeric_limits<int>::max();
+    node1 = 0;
+    node2 = 0;
 
     for (int i = 0; i < numPoints; i++) {
-      if (selected[i]) {
+      if (handled[i]) {
         // cout << "selected[i]: " << i << endl;
         for (int j = 0; j < numPoints; j++) {
-          if (!selected[j] && weight[i][j] && i != j) {  // not in selected and there is an edge
+          if (!handled[j] && weight[i][j]) {  // not in selected and there is an edge
             // cout << "selected[j] not selected found: " << j << endl;
             if (min > weight[i][j] && i != j) {
             //  cout << "new min: " << weight[i][j] << " between " << i << " and " << j << endl;
               min = weight[i][j];
-              x = i;
-              y = j;
+              node1 = i;
+              node2 = j;
             }
           }
         }
       }
     }
-    cout << x << " - " << y << " :  " << weight[x][y];
+    
+    adjacencyList[node1].push_back(node2);
+    adjacencyList[node2].push_back(node1);
+    //cout << node1 << " - " << node2 << " :  " << weight[node1][node2];
     cout << endl;
-    selected[y] = true;
-    no_edge++;
+    handled[node2] = true;
+    numEdges++;
   }
 
-  return 0;    
+  return adjacencyList;    
 }
 
 
@@ -105,7 +88,7 @@ int main() {
     // Read input
     int N;
     cin >> N;
-    cout << "heklk" << endl;
+    
     vector<double> inner_vector(N);
     vector<vector<double> > weight(N,inner_vector);
     weight = receiveInput(N);
@@ -116,8 +99,17 @@ int main() {
         cout << endl;
     }
     
-    cout << "N is " << N << endl;
-    prims(weight, N);
+    vector<vector<int> > adjacencyList(N);
+    adjacencyList = prims(weight, N);
+
+    // Print adjacency list
+    // for (int i = 0; i < N; ++i) {
+    //     cout << i << ": ";
+    //     for (int j = 0; j < adjacencyList[i].size(); ++j) {
+    //         cout << adjacencyList[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
 
     return 0;
 }
