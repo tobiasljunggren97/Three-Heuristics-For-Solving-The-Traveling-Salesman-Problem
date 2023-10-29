236cf6601f3563e87d8502d3aa35c2d7ecd64eef
Vendela Asplund <venna.asplund@hotmail.com>
2023-10-29 14:15:22 +0100

    removed buildVendela

--- buildVendela/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/venna/Documents/KTH/year4/DD2440/TSPAvalg
-# Build directory: C:/Users/venna/Documents/KTH/year4/DD2440/TSPAvalg/buildVendela
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("test")
--- buildVendela/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/venna/Documents/KTH/year4/DD2440/TSPAvalg/test
-# Build directory: C:/Users/venna/Documents/KTH/year4/DD2440/TSPAvalg/buildVendela/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestingSuite "C:/Users/venna/Documents/KTH/year4/DD2440/TSPAvalg/buildVendela/test/TestingSuite.exe")
-set_tests_properties(TestingSuite PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/venna/Documents/KTH/year4/DD2440/TSPAvalg/test/CMakeLists.txt;17;add_test;C:/Users/venna/Documents/KTH/year4/DD2440/TSPAvalg/test/CMakeLists.txt;0;")
