6bec4efaa4a9f99dc6fce8924b51493c99de7933
tljun <tljun@kth.se>
2023-11-03 16:48:21 +0100

    Now printing results correctly in main, also all prints in code commented out! And tsp has got new return value, calculates cost directly

--- main.cpp
+++ main.cpp
@@ -4,13 +4,24 @@
 #include <iomanip>
 #include <tuple>
 #include "main.h"
+#include "TSPSolution.h"
 
 using namespace std;
+
+void printSolution(TSPSolution &solution, Graph &g)
+{
+    int n = solution.tour.size();
+    for(int i = 0; i < n; i++)
+    {
+        cout << solution.tour[i] << endl;
+    }
+}
 
 int main() {
     //Initializing Graph will read input and create weight matrix
     Graph g = Graph();
-    christofides(g);  
+    TSPSolution christofidesSolution = christofides(g);  
+    printSolution(christofidesSolution, g);
     return 0;
 }
 
